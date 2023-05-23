f359b9a586262e71c99e1c71a2ef4a30a1930490
poorhorse <howlinblindowl@live.com>
2023-05-23 10:10:47 -0700

    win in caps

--- app.js
+++ app.js
@@ -11,10 +11,10 @@ function updateScores(player, points) {
    document.getElementById("computerScore").textContent = computerScore;
    
    if (playerScore >= 5) {
-     document.getElementById("winnerFinal").textContent = "Player Wins!";
+     document.getElementById("winnerFinal").textContent = "PLAYER WINS!";
      endGame();
    } else if (computerScore >= 5) {
-     document.getElementById("winnerFinal").textContent = "Computer Wins!";
+     document.getElementById("winnerFinal").textContent = "COMPUTER WINS!";
      endGame();
    }
  }
@@ -24,24 +24,24 @@ function updateScores(player, points) {
  
    if (playerSelection === "rock" && computerSelection === "paper") {
      updateScores(2, 1);
-     document.getElementById("winner").textContent = "You Lose! Paper beats Rock!";
+     document.getElementById("winner").textContent = "You lose. Paper beats rock.";
    } else if (playerSelection === "paper" && computerSelection === "scissors") {
      updateScores(2, 1);
-     document.getElementById("winner").textContent = "You Lose! Scissors beats Paper!";
+     document.getElementById("winner").textContent = "You lose. Scissors beats paper.";
    } else if (playerSelection === "scissors" && computerSelection === "rock") {
      updateScores(2, 1);
-     document.getElementById("winner").textContent = "You Lose! Rock beats Scissors!";
+     document.getElementById("winner").textContent = "You lose. Rock beats scissors.";
    } else if (playerSelection === "rock" && computerSelection === "scissors") {
      updateScores(1, 1);
-     document.getElementById("winner").textContent = "You Win! Rock beats Scissors!";
+     document.getElementById("winner").textContent = "You win. Rock beats scissors.";
    } else if (playerSelection === "paper" && computerSelection === "rock") {
      updateScores(1, 1);
-     document.getElementById("winner").textContent = "You Win! Paper beats Rock!";
+     document.getElementById("winner").textContent = "You win. Paper beats rock.";
    } else if (playerSelection === "scissors" && computerSelection === "paper") {
      updateScores(1, 1);
-     document.getElementById("winner").textContent = "You Win! Scissors beats Paper!";
+     document.getElementById("winner").textContent = "You win. Scissors beats paper.";
    } else {
-     document.getElementById("winner").textContent = "It's a tie!";
+     document.getElementById("winner").textContent = "It's a tie.";
    }
  }
  
@@ -65,6 +65,39 @@ function updateScores(player, points) {
    console.log("You picked scissors!");
    playRound("scissors", getComputerChoice());
  });
+
+ const resetButton = document.getElementById("resetButton");
+
+resetButton.addEventListener("click", function() {
+  // Reset the game logic
+  playerScore = 0;
+  computerScore = 0;
+  document.getElementById("playerScore").textContent = playerScore;
+  document.getElementById("computerScore").textContent = computerScore;
+  document.getElementById("winnerFinal").textContent = "";
+  document.getElementById("winner").textContent = "";
+
+  // Enable the game buttons
+  paperButton.disabled = false;
+  rockButton.disabled = false;
+  scissorsButton.disabled = false;
+
+  // Hide the reset button again
+  resetButton.style.display = "none";
+});
+
+function endGame() {
+  document.getElementById("winnerFinal").textContent = "GAME OVER";
+  document.getElementById("winner").textContent = "";
+
+  paperButton.disabled = true;
+  rockButton.disabled = true;
+  scissorsButton.disabled = true;
+
+  // Show the reset button
+  resetButton.style.display = "block";
+}
+
  
  let playerScore = 0;
  let computerScore = 0;
@@ -81,6 +114,8 @@ function updateScores(player, points) {
    paperButton.disabled = true;
    rockButton.disabled = true;
    scissorsButton.disabled = true;
-   
+   document.getElementById("resetButton").style.display = "block";
+
+
  }
  
